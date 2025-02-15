776b960438f9fe5fa1047e333fb50e9ba625a7f7
Adam <mikayilov.adam@gmail.com>
2025-02-15 20:13:46 +0400

    push

---
+++ app.js
@@ -0,0 +1,22 @@
+let N1 = 6;
+let N2 = 6;
+
+
+
+function minus(){
+    document.getElementById('huf').innerHTML = N1-N2;
+}
+
+function bolme(){
+    document.getElementById('huf').innerHTML = N1/N2;
+}
+
+
+function plus(){
+    document.getElementById('huf').innerHTML = N1+N2;
+}
+
+
+function vurma(){
+    document.getElementById('huf').innerHTML = N1*N2;
+}
